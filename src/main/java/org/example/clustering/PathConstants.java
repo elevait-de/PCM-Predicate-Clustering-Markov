2a11a3a1f4d8ad8b273037e36a4bd3512113ac3e
Asal Alikhani <asal.alikhani@elevait.de>
2023-04-13 18:19:12 +0200

    added static PATH to the beginning of the other paths in PathConstants file

--- src/main/java/org/example/clustering/PathConstants.java
+++ src/main/java/org/example/clustering/PathConstants.java
@@ -4,15 +4,15 @@ public class PathConstants {
 
     private PathConstants() {}
 
-    static final String PATH = "src/main/resources/";
-    public static final String MEASUREMENT_PATH = PATH + "/measurements.csv";
-    public static final String QUERIES_PATH = "src/main/resources/input/queries/2014-05-19.csv";
-    public static final String DATASET_PATH = "src/main/resources/input/datasets/SWDF.nt";
-    static final String CLUSTER_FILE = PATH +"clusters.txt";
-    static final String PREDICATE_FILE = PATH +"predicateEncoding.txt";
-    static final String PREDICATE_FILES = PATH +"predicate-based/";
-    static final String PARTITION_OUTPUT_FOLDER = PATH +"final-partitions/";
-    static final String GRAPH_WEIGHT_FILE = PATH + "/graphweight.txt";
+    static final String PATH = "/home/asal/Documents/3DFed/Elevait-Experimental-Result-3Aril23/";
+    public static final String MEASUREMENT_PATH = PATH + "pcm/pcm-partition-files/fully-featured-queries/pcm-measurements.csv";
+    public static final String QUERIES_PATH = PATH + "queryLogs/SWDF/train-querylog-csv-files/fully-featured-queries/sparql_2023-04-06_09-15-50Z-9.csv"; //swdf-300-bgp-queries";
+    public static final String DATASET_PATH = PATH + "datasets/SWDF.nt";
+    static final String CLUSTER_FILE = PATH +"pcm/clusters.txt";
+    static final String PREDICATE_FILE = PATH +"pcm/predicateEncoding.txt";
+    static final String PREDICATE_FILES = PATH +"pcm/predicate-based/";
+    static final String PARTITION_OUTPUT_FOLDER = PATH +"pcm/pcm-partition-files/fully-featured-queries/sparql_2023-04-06_09-15-50Z-9"; //swdf-300-bgp-queries";
+    static final String GRAPH_WEIGHT_FILE = PATH + "pcm/graphweight.txt";
     static final int TOTAL_PARTITIONS = 10;
 
     //dbpedia dataset has total 232536510 triples
